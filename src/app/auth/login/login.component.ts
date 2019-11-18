db4bf02a078c05aa5c3581bdc33dc23b6d08e81e
parminder singh <parminder@datiot.com>
2019-11-18 12:23:07 +0530

    channges done

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  styleUrls: ['./login.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
